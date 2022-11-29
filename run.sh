6f690b09ecb996a6e5bd0f8cbf278b17f1e18ce0
沐泽 <111378049+MUZEQAQ@users.noreply.github.com>
2022-11-29 10:34:19 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,4 @@ cd `dirname $0`
 PATH=$PATH:/usr/local/bin
 # 休眠随机时间0-99s
 #sleep $[$RANDOM%100]
-python3 bilibili.py >> data.txt
-echo "date,follower" > data.csv
-tac data.txt >> data.csv
+python3 Refresh.py >> index.html
